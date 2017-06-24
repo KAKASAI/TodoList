eb1a7aa527e03fd65d3c28d587701a53a2404d59
KAKASAI <djbillyms59@gmail.com>
2017-06-24 23:33:59 +0800

    Ver0.1
    fix delet button

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function initialData(e) {
     // console.log(dataArray);
     let missionListAll = '';
     for (let i = 0; i < dataArray.length; i++) {
-        missionListAll += '<li data-num = "' + i + '" class="missionList">' + dataArray[i] + '</li>';
+        missionListAll += '<li class="missionList">' + dataArray[i] + '<button data-num="' + i + '"class="btnDel">刪除</button></li>';
         // console.log(missionListAll);
     }
     missionUnit.innerHTML = missionListAll;
@@ -35,10 +35,10 @@ function checkMission(e) {
 //點選任務刪除
 function delList(e) {
     // console.log(e.target.dataset.num);
-    // console.log(e.target.nodeName)
+    console.log(e.target.nodeName)
     let num = e.target.dataset.num;
-    if (e.target.nodeName == 'LI') {
-        // console.log(dataArray[num]);
+    if (e.target.nodeName == 'BUTTON') {
+        console.log(num);
         dataArray.splice(num, 1);
         // console.log(dataArray);
         localStorage.setItem('mission', JSON.stringify(dataArray));
